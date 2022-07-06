a8113dbead664a75b4966c540c1a83c9c1e31639
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-06 21:46:16 +0300

    add domain money class

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,58 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+//immutable value object
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.amount != null && amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money){
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money)
+    {
+        return new Money(setScale(this.amount.add(money.getAmount())));
+    }
+
+    public Money subtract(Money money)
+    {
+        return new Money(setScale(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money multiply(int multiplier)
+    {
+        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
+    }
+
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_DOWN);
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
