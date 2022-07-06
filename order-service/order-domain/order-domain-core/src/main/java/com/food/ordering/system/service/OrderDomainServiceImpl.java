a15c21bfde2183ed80d82b0bbde486f9811a5cf5
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-07 02:36:57 +0300

    set order domain service implementation

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/OrderDomainServiceImpl.java
@@ -0,0 +1,67 @@
+package com.food.ordering.system.service;
+
+import com.food.ordering.system.service.domain.OrderDomainService;
+import com.food.ordering.system.service.domain.entity.Order;
+import com.food.ordering.system.service.domain.entity.Product;
+import com.food.ordering.system.service.domain.entity.Restaurant;
+import com.food.ordering.system.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.service.domain.event.OrderPaidEvent;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    @Override
+    public OrderCreatedEvent validateInitializeOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order,restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} has been initialized", order.getId());
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of("UTC")));
+    }
+
+    //for cross check
+    //n squared time complexity
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct ->{
+            Product currentProduct = orderItem.getProduct();
+
+            if (currentProduct.equals(restaurantProduct)) {
+               currentProduct.updateWithConfirmedNameAndPrice(restaurantProduct.getName(),
+                                                              restaurantProduct.getPrice());
+            }
+        }));
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new IllegalArgumentException("Restaurant with id: " + restaurant.getId().getValue() + " is currently not active");
+        }
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        return null;
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+
+    }
+}
