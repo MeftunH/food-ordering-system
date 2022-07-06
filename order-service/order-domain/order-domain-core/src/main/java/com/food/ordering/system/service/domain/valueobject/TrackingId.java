18b32174f43d5fac499f86e01db14b0ee0ff9893
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-06 22:52:29 +0300

    set tracking id value object class

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/domain/valueobject/TrackingId.java
@@ -0,0 +1,11 @@
+package com.food.ordering.system.service.domain.valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+    public TrackingId(UUID value) {
+        super(value);
+    }
+}
