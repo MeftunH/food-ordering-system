22a5965ac8b55c340b6f18515d8ac5012aaa8035
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-07 05:00:27 +0300

    customer repo function parameter bug fix

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
@@ -3,7 +3,8 @@ package com.food.ordering.system.order.service.domain.ports.output.repository;
 import com.food.ordering.system.service.domain.entity.Customer;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CustomerRepository {
-    Optional<Customer> findCustomer(Customer customer);
+    Optional<Customer> findCustomer(UUID customerId);
 }
