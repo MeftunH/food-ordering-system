281a6e580a11436a390c6f4f950b6436c68e9925
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-07 05:00:01 +0300

    set order application service class

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/OrderApplicationServiceImpl.java
@@ -0,0 +1,38 @@
+package com.food.ordering.system.order;
+
+import com.food.ordering.system.order.service.domain.OrderCreateCommandHandler;
+import com.food.ordering.system.order.service.domain.OrderTrackCommandHandler;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+class OrderApplicationServiceImpl implements OrderApplicationService {
+
+    private final OrderCreateCommandHandler orderCreateCommandHandler;
+    private final OrderTrackCommandHandler orderTrackCommandHandler;
+
+    public OrderApplicationServiceImpl(OrderCreateCommandHandler orderCreateCommandHandler, OrderTrackCommandHandler orderTrackCommandHandler) {
+        this.orderCreateCommandHandler = orderCreateCommandHandler;
+        this.orderTrackCommandHandler = orderTrackCommandHandler;
+    }
+
+
+    @Override
+    public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
+
+        return orderCreateCommandHandler.createOrder(createOrderCommand);
+    }
+
+    @Override
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        return orderTrackCommandHandler.trackOrder(trackOrderQuery);
+    }
+}
