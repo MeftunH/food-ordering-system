b5230740ae476a89fcc494a795fc2e6bfdbe1201
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-07 03:39:53 +0300

    set order paid restaurant request message publisher

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.service.domain.event.OrderPaidEvent;
+
+public interface OrderPaidRestaurantRequestMessagePublisher extends DomainEventPublisher<OrderPaidEvent> {
+
+}
