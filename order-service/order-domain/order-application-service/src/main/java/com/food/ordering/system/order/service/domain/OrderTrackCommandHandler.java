d039b6cd2c07e71ba830123a71608d330da01a12
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-02 05:49:03 +0300

    set Order Track Command Handler

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,14 +2,38 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.service.domain.entity.Order;
+import com.food.ordering.system.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.service.domain.valueobject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
 
-    TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
+    private final OrderDataMapper orderDataMapper;
 
+    public OrderTrackCommandHandler(OrderDataMapper orderDataMapper, OrderRepository orderRepository) {
+        this.orderDataMapper = orderDataMapper;
+        this.orderRepository = orderRepository;
     }
+
+    private final OrderRepository orderRepository;
+    @Transactional(readOnly = true)
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        Optional<Order> orderResult = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+
+        if (orderResult.isEmpty()) {
+            log.warn("Order not found by given tracking id: {}", trackOrderQuery.getOrderTrackingId());
+            throw new OrderDomainException("Order not found by given tracking id: " + trackOrderQuery.getOrderTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
+    }
+
 }
